25fd35c9fb4cb1d837ff23d7f8cf320adc8a9dcb
Pedro Brandão <brandaopj@gmail.com>
2026-05-15 15:41:21 +0100

    chore: configure vitest with allure-vitest reporter

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    reporters: process.env.CI
+      ? [['allure-vitest/reporter', { resultsDir: 'allure-results' }]]
+      : ['verbose'],
+  },
+})
